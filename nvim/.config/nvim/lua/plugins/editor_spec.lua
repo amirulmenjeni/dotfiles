6af11223cd2ed9204bc28b96a5592d2de5f6f233
amirulmenjeni <amirulmenjeni@gmail.com>
2024-07-27 20:07:19 +0800

    Update navigation key mapping

--- nvim/.config/nvim/lua/plugins/editor_spec.lua
+++ nvim/.config/nvim/lua/plugins/editor_spec.lua
@@ -12,7 +12,7 @@ return {
         lazy = false,
         priority = 2500,
         config = function()
-            vim.keymap.set('n', '<C-p>', ':NvimTreeToggle<CR>')
+            vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
 
             local function my_on_attach(bufnr)
                 local api = require "nvim-tree.api"
@@ -104,8 +104,8 @@ return {
         },
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            vim.keymap.set('n', '<S-f>', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         end,
