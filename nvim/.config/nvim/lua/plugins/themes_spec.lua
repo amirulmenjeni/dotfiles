4331c81a15a50a83217a672caf4796caa13b13e6
amirulmenjeni <amirulmenjeni@gmail.com>
2024-07-27 20:01:10 +0800

    Use dracula theme as default

--- nvim/.config/nvim/lua/plugins/themes_spec.lua
+++ nvim/.config/nvim/lua/plugins/themes_spec.lua
@@ -1,10 +1,30 @@
 return {
     {
-        "folke/tokyonight.nvim",
+        "dracula/vim",
         lazy = false,
         priority = 1000,
         config = function()
+            vim.cmd([[colorscheme dracula]])
+            vim.cmd([[syntax enable]])
+        end,
+        enable = true,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1500,
+        config = function()
             vim.cmd([[colorscheme tokyonight]])
         end,
+        enable = false,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 2000,
+        config = function()
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+        enable = true,
     },
 }
