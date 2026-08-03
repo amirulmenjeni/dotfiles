4066e59e5e481cffc59a54e35fda9feda4bf9c69
Amirul Menjeni <amirulmenjeni@macos.local>
2026-08-03 14:33:43 +0800

    feat(nvim): add display-line jump mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -211,6 +211,9 @@ do
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+  vim.keymap.set({ 'n', 'v' }, 'K', '10gk', { desc = 'Move up 10 display lines' })
+  vim.keymap.set({ 'n', 'v' }, 'J', '10gj', { desc = 'Move down 10 display lines' })
+
   -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
   -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
   -- is not what someone will guess without a bit more experience.
