9a8344891c9874346df21c5c0486c8c8c19c3219
Amirul Menjeni <amirulmenjeni@macos.local>
2026-08-14 22:29:58 +0800

    Enable relative line numbering

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -119,9 +119,8 @@ do
 
   -- Make line numbers default
   vim.o.number = true
-  -- You can also add relative line numbers, to help with jumping.
-  --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
+  -- Use relative line numbers to help with jumping.
+  vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
